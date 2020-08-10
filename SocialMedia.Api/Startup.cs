b9fa71a8fb0c2b8e08f7ca229ecc00bb6a7a5803
Nicolas <nicolasecolman@gmail.com>
2020-08-10 15:02:24 -0300

    Implementación Entity Framework.

--- SocialMedia.Api/Startup.cs
+++ SocialMedia.Api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +29,10 @@ namespace SocialMedia.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<Infrastructure.Data.SocialMediaContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("SocialMedia")));
+
             services.AddTransient<IPostRepository, PostRepository>();
         }
 
