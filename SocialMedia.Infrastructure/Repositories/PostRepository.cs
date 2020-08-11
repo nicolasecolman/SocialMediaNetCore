828e0c8732b6a8897e83c03abeda26a5f7d61c8c
Nicolas <nicolasecolman@gmail.com>
2020-08-11 15:06:24 -0300

    Entidades en Inglés.

--- SocialMedia.Infrastructure/Repositories/PostRepository.cs
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -14,9 +14,9 @@ namespace SocialMedia.Infrastructure.Repositories
         {
             _context = socialMediaContext;
         }
-        public async Task<IEnumerable<Publicacion>> GetPosts()
+        public async Task<IEnumerable<Post>> GetPosts()
         {
-            var posts = await _context.Publicacion.ToListAsync();
+            var posts = await _context.Posts.ToListAsync();
 
             return posts;
         }
