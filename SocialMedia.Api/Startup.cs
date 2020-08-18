bbfe5c1e679269de3919e12e42beb633c6dffc46
Nicolas <nicolasecolman@gmail.com>
2020-08-18 11:22:03 -0300

    Migraciópn a NetCore 3.1. Instalación de Microsoft.AspNetCore.Mvc.NewtonsoftJson.

--- SocialMedia.Api/Startup.cs
+++ SocialMedia.Api/Startup.cs
@@ -27,6 +27,11 @@ namespace SocialMedia.Api
                 options.UseSqlServer(Configuration.GetConnectionString("SocialMedia")));
 
             services.AddTransient<IPostRepository, PostRepository>();
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
