7d6f07e278106c417d98394a825ff411210abde0
Nicolas <nicolasecolman@gmail.com>
2020-08-18 18:24:13 -0300

    Conversión manual entre entidades y DTOs.

--- SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Api.Controllers
@@ -18,20 +20,44 @@ namespace SocialMedia.Api.Controllers
         public async Task<IActionResult> GetPosts()
         {
             var posts = await _postRepository.GetPosts();
-            return Ok(posts);
+            var postsDto = posts.Select(p => new PostDto
+            {
+                PostId = p.PostId,
+                Date = p.Date,
+                Description = p.Description,
+                Image = p.Image,
+                UserId = p.UserId
+            });
+            return Ok(postsDto);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPost(int id)
         {
             var post = await _postRepository.GetPost(id);
-            return Ok(post);
+            var postDto = new PostDto
+            {
+                PostId = post.PostId,
+                Date = post.Date,
+                Description = post.Description,
+                Image = post.Image,
+                UserId = post.UserId
+            };
+            return Ok(postDto);
         }
 
 
         [HttpPost]
-        public async Task<IActionResult> Post(Post post)
+        public async Task<IActionResult> Post(PostDto postDto)
         {
+
+            var post = new Post
+            {
+                Date = postDto.Date,
+                Description = postDto.Description,
+                Image = postDto.Image,
+                UserId = postDto.UserId
+            };
             await _postRepository.InsertPost(post);
             return Ok(post);
         }
